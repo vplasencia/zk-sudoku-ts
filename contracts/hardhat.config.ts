86d0fa0b7438ee09ba0e5ca739c6639e3c1ade35
Vivian Plasencia <v.pcalana@gmail.com>
2025-03-09 22:56:37 +0100

    feat: add gas report

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-gas-reporter"
+import { config as dotenvConfig } from "dotenv"
 
-if (!process.env.PRIVATE_KEY) {
+dotenvConfig();
+
+if (process.env.NODE_ENV !== "development" && !process.env.PRIVATE_KEY) {
     console.error("Please set your PRIVATE_KEY in a .env file")
     process.exit(1)
 }
@@ -22,10 +26,16 @@ const config: HardhatUserConfig = {
             allowUnlimitedContractSize: true
         },
         sepolia: {
+            url: "https://rpc2.sepolia.org",
             chainId: 11155111,
-            accounts: [process.env.PRIVATE_KEY]
+            accounts: process.env.NODE_ENV !== "development" && !process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY!] : []
         }
-    }
+    },
+    gasReporter: {
+        currency: "USD",
+        enabled: process.env.REPORT_GAS === "true",
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY
+    },
 }
 
 export default config
