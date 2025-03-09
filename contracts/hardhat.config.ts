8f2d2f816a952502d9d2d39d8c562d30ae7cef99
Vivian Plasencia <v.pcalana@gmail.com>
2025-03-09 22:36:48 +0100

    feat: add sepolia as a network in contracts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
 
+if (!process.env.PRIVATE_KEY) {
+    console.error("Please set your PRIVATE_KEY in a .env file")
+    process.exit(1)
+}
+
 const config: HardhatUserConfig = {
-    solidity: "0.8.28"
+    solidity: {
+        version: "0.8.28",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+    networks: {
+        hardhat: {
+            chainId: 1337,
+            allowUnlimitedContractSize: true
+        },
+        sepolia: {
+            chainId: 11155111,
+            accounts: [process.env.PRIVATE_KEY]
+        }
+    }
 }
 
 export default config
